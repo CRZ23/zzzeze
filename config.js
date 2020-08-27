b3d29cd7bee08b06ec6510adbbfc5e36316bbb66
CRZ23 <56548752+CRZ23@users.noreply.github.com>
2020-08-28 00:32:18 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NzQ4NjY2OTMyODg2MjQxNDUz.X0gwag.Ej7GJ4wLpm7V3YU8nEEcUZ1Fp7k', /// توكن البوت
+    YT_API_KEY: 'api ', /// مش مهم
+    prefix: '#', // هنا برفكس او مقدمة البوت
+    devs: ['ايدي حسابك'] //// ايدي حسابك
+}
